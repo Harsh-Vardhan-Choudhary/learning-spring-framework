a920ec32f838e916daabd6821b0fb265ea05e590
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-03-17 14:38:15 +0530

    AppGamingBasicJava class

--- src/main/java/com/harsh/learningspringframework/AppGamingBasicJava.java
+++ src/main/java/com/harsh/learningspringframework/AppGamingBasicJava.java
@@ -5,13 +5,14 @@ import com.harsh.learningspringframework.game.MarioGame;
 import com.harsh.learningspringframework.game.PacmanGame;
 import com.harsh.learningspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
-
-	public static void main(String[] args) {
-		//var marioGame = new MarioGame();
-		var superContraGame = new SuperContraGame();
-		var gameRunner = new GameRunner(superContraGame);
+public class AppGamingBasicJava 
+{
+	public static void main(String[] args)
+	{
+		//var game = new MarioGame();
+		//var game = new SuperContraGame();
+		var game = new PacmanGame();
+		var gameRunner = new GameRunner(game);
 		gameRunner.run();
 	}
-
 }
