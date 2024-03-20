242e468feb0c77fb97ed3f46ca62f0128fe7b7ff
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-03-20 22:13:02 +0530

    Merging GamingConfiguration file in App2GamingSpringBean

--- src/main/java/com/harsh/learningspringframework/App2GamingSpringBean.java
+++ src/main/java/com/harsh/learningspringframework/App2GamingSpringBean.java
@@ -4,12 +4,33 @@ import java.text.Annotation;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.harsh.learningspringframework.game.GameRunner;
 import com.harsh.learningspringframework.game.GamingConsole;
 import com.harsh.learningspringframework.game.MarioGame;
 import com.harsh.learningspringframework.game.PacmanGame;
 import com.harsh.learningspringframework.game.SuperContraGame;
+
+@Configuration
+class GamingConfiguration 
+{
+    @Bean
+    public GamingConsole game()
+    {
+        var game = new PacmanGame();
+        return game;
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game)
+    {
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+    
+}
 
 public class App2GamingSpringBean 
 {
