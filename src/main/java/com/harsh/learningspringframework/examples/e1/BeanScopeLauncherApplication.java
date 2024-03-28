7531ceb0015056aa9ee0146816655f3ea1419ab5
Harsh Vardhan Choudhary <choudharyharsh964@gmail.com>
2024-03-28 23:28:57 +0530

    Bean Scopes

--- src/main/java/com/harsh/learningspringframework/examples/e1/BeanScopeLauncherApplication.java
+++ src/main/java/com/harsh/learningspringframework/examples/e1/BeanScopeLauncherApplication.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -34,7 +33,12 @@ public class BeanScopeLauncherApplication
 	{
 		try (var context = new AnnotationConfigApplicationContext(BeanScopeLauncherApplication.class)) 
 		{
-			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
 		} 
 	}
 }
